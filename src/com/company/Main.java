4b2fbfe0d082f83f0200739ed6594cf581346720
annanovikova614 <72442702+annanovikova614@users.noreply.github.com>
2020-10-14 19:59:32 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,44 +5,55 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args)
-    { 
-        printArea ();
-    }
-    public static void printArea() 
     {
-        System.out.println("Значение площади закрашенной фигуры = " + calculateArea(readRadiusOfMinorCircle(), readRadiusOfGreatCircle()));
+        outputAreaFilledShape();
     }
-    public static int readRadiusOfMinorCircle()
+
+    public static void outputAreaFilledShape()
+    {
+        double radius1 = readRadiusOfMinorCircle();
+        double radius2 = readRadiusOfBigCircle();
+        double area = calculateArea(radius1, radius2);
+        System.out.println("Значение площади закрашенной фигуры = " + area);
+    }
+
+    public static double readRadiusOfMinorCircle()
     {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите радиус малой окружности:");
-        double r1 = scanner.nextDouble();
-        return (int) r1;
+        double radius1 = scanner.nextDouble();
+        return radius1;
     }
-    public static int readRadiusOfGreatCircle()
+
+    public static double readRadiusOfBigCircle()
     {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите радиус большой окружности:");
-        double r2 = scanner.nextDouble();
-        return (int) r2;
+        double radius2 = scanner.nextDouble();
+        return radius2;
     }
-    static double calculateArea(double r1, double r2)
+
+    static double calculateArea(double radius1, double radius2)
     {
-        double area1 = calculateArea1(r2, r1);
-        double area2 = calculateArea2(r1);
-        double area3 = calculateArea3(r2);
-        return area1 + area2 + area3;
+        double area1 = calculateArea1(radius2, radius1);
+        double area2 = calculateArea2(radius1);
+        double area3 = calculateArea3(radius2);
+        double area = area1 + area2 + area3;
+        return area;
     }
-    static double calculateArea1(double r1, double r2)
+
+    static double calculateArea1(double radius1, double radius2)
     {
-        return (Math.PI * r2 * r2 - Math.PI * r1 * r1) * 0.25;
+        return (Math.PI * radius2 * radius2 - Math.PI * radius1 * radius1) * 0.25;
     }
-    static double calculateArea2(double r1)
+
+    static double calculateArea2(double radius1)
     {
-        return Math.PI * r1 * r1 * 0.375;
+        return Math.PI * radius1 * radius1 * 0.375;
     }
-    static double calculateArea3(double r2)
+
+    static double calculateArea3(double radius2)
     {
-        return (4 * r2 * r2 - Math.PI * r2 * r2) * 0.125;
+        return (4 * radius2 * radius2 - Math.PI * radius2 * radius2) * 0.125;
     }
 }
