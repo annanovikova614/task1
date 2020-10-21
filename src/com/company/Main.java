c7284aad43926c5da8a9f23bdfb8d22209c21c7b
annanovikova614 <72442702+annanovikova614@users.noreply.github.com>
2020-10-21 20:07:09 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,62 +5,43 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Значение площади закрашенной фигуры = " + areaFilledShape());
+        double radius1 = readDouble("меньший радиус");
+        double radius2 = readDouble("больший радиус");
+        double result = calculateAreaOfFilledShape(radius1, radius2);
+        printResultOfCalculation(result);
     }
 
-    public static double readRadiusOfMinorCircle() {
+    private static double readDouble(String string) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите радиус малой окружности:");
-        double radius1 = scanner.nextDouble();
-        return radius1;
+        System.out.printf("Введите %s: ", string);
+        return scanner.nextDouble();
     }
 
-    public static double readRadiusOfBigCircle() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите радиус большой окружности:");
-        double radius2 = scanner.nextDouble();
-        return radius2;
+    private static void printResultOfCalculation(double result) {
+        System.out.printf("Площадь закрашенной фигуры = %.1f", result);
     }
 
-    public static double minorCircleS(double radius1) {
-        return Math.PI * radius1 * radius1;
+    private static double calculateAreaOfFilledShape(double radius1, double radius2) {
+        return calculateArea1(radius1, radius2) + calculateArea2(radius1) + calculateArea3(radius2);
     }
 
-    public static double bigCircleS(double radius2) {
-        return Math.PI * radius2 * radius2;
+    private static double calculateArea1(double radius1, double radius2) {
+        return (calculateAreaOfCircle(radius2) - calculateAreaOfCircle(radius1)) / 4;
     }
 
-    public static double squareS(double radius2) {
-        double side = 2 * radius2;
+    private static double calculateArea2(double radius1) {
+        return calculateAreaOfCircle(radius1) / 8 * 3;
+    }
+
+    private static double calculateArea3(double radius2) {
+        return (calculateAreaOfSquare(2 * radius2) - calculateAreaOfCircle(radius2)) / 8;
+    }
+
+    private static double calculateAreaOfCircle(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    private static double calculateAreaOfSquare(double side) {
         return side * side;
     }
-
-    static double calculateArea1(double minorcircleS) {
-        return minorcircleS / 8 * 3;
-    }
-
-    static double calculateArea2(double minorcircleS, double bigcircleS) {
-        return (bigcircleS - minorcircleS) / 8 * 2;
-    }
-
-    static double calculateArea3(double bigcircleS, double squareS) {
-        return (squareS - bigcircleS) / 8 * 1;
-    }
-
-    public static double calculateArea(double area1, double area2, double area3) {
-        return area1 + area2 + area3;
-    }
-
-    public static double areaFilledShape() {
-        double radiusMinor = readRadiusOfMinorCircle();
-        double radiusBig = readRadiusOfBigCircle();
-        double minorcircleS = minorCircleS(radiusMinor);
-        double bigcircleS = bigCircleS(radiusBig);
-        double square = squareS(radiusBig);
-        double area1 = calculateArea1(minorcircleS);
-        double area2 = calculateArea2(minorcircleS, bigcircleS);
-        double area3 = calculateArea3(bigcircleS, square);
-        return calculateArea(area1, area2, area3);
-    }
 }
-
